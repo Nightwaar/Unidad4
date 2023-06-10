4b9465c8be7dcb007df2e88bf10ce6acc0f7e07a
MarcosMolina <90269496+Nightwaar@users.noreply.github.com>
2023-06-10 18:58:35 -0300

    1

--- EJ2/calculoiva.py
+++ EJ2/calculoiva.py
@@ -4,35 +4,46 @@ from tkinter import ttk
 class calculariva():
     __ventana=None
     __preciobase=None
+    __precioiva=None
     def __init__(self):
         self.__ventana=Tk()
-        self.__ventana.geometry('500x500')
+        self.__ventana.geometry('400x400')
         self.__ventana.configure(bg='white')
         self.__ventana.title('Calculador de iva')
-        iva1=int(21)
-        iva2=float(10.5)
-        
+        self.iva=DoubleVar()
         opts = { 'ipadx': 500, 'ipady': 10}
         ttk.Label(self.__ventana,text='Calculo de IVA',background='gray').place(width=500,height=50)
         
-        ttk.Label(self.__ventana,text='IVA 21%').place(x=150,y=150)
-        ttk.Radiobutton(self.__ventana,textvariable=iva1).place(x=130,y=150)
-        ttk.Label(self.__ventana,text='IVA 10.5%').place(x=150,y=180)
-        ttk.Radiobutton(self.__ventana,textvariable=iva2).place(x=130,y=180)
         style = ttk.Style()
         style.configure("Red.TButton", background="red")
-        ttk.Button(self.__ventana, text='Salir', style="Red.TButton", command=self.__ventana.destroy).place(x=400, y=425)
+        ttk.Button(self.__ventana, text='Salir', style="Red.TButton", command=self.__ventana.destroy).place(x=300, y=325)
         style1 = ttk.Style()
         style1.configure("Green.TButton", background="green")
-        ttk.Button(self.__ventana, text='Calcular', style="Green.TButton", command=self.calcular).place(x=200, y=425)
+        ttk.Button(self.__ventana, text='Calcular', style="Green.TButton", command=self.calcular).place(x=80, y=325)
         
         ttk.Label(self.__ventana,text='Precio sin IVA').place(x=80,y=90)
-        self.__preciobase=StringVar()
+        self.__preciobase=DoubleVar()
+        self.__precioiva=DoubleVar()
         self.preciobaseEntry=ttk.Entry(self.__ventana,textvariable=self.__preciobase).place(x=180,y=90)
+        
+        
+        seleccionar=ttk.LabelFrame(self.__ventana,text="Seleccione el IVA del producto").place(x=110,y=140)
+        ttk.Radiobutton(seleccionar,text="IVA 21%",value=0,variable=self.iva,command=self.calcular()).place(x=130,y=180)
+        ttk.Radiobutton(seleccionar,text="IVA 10.5%",value=1,variable=self.iva,command=self.calcular()).place(x=130,y=220)
+        
+        ttk.Label(self.__ventana,text="Precio con IVA: ").place(x=100,y=260)
+        ttk.Label(self.__ventana,textvariable=self.__precioiva).place(x=200,y=260)
         
         self.__ventana.mainloop()
     def calcular(self):
-        ttk.Label(self.__ventana,text='Hola').place(x=300,y=290)
+        valor=float(self.__preciobase.get())
+        if self.iva.get()==0:
+            iva=valor*21/100
+            self.__precioiva.set(valor+iva)
+        else:
+            if self.iva.get()==1:
+                iva=valor*10.5/100
+                self.__precioiva.set(valor+iva)
 def testapp():
     miapp=calculariva()
 if __name__=='__main__':
