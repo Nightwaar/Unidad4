5d06eabc12361b3a94ae246666041c2c6bb393d8
MarcosMolina <90269496+Nightwaar@users.noreply.github.com>
2023-06-11 23:44:29 -0300

    Incompleto

---
+++ EJ4/ClaseImaginario.py
@@ -0,0 +1,13 @@
+
+from tkinter import *
+from tkinter import ttk
+from functools import partial
+from calculadorTkinter import Calculadora
+class imaginario:
+    def __add__(self):
+        
+    def __sub__(self):
+    
+    def __mul__(self):
+        
+    def __div__(Self):
